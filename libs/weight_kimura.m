1148d0321151283cceddfa0801f8bf31ca810a79
leoleoleo17 <leo17312304@gmail.com>
2024-05-13 21:08:03 -0500

    Update weight_kimura.m

--- libs/weight_kimura.m
+++ libs/weight_kimura.m
@@ -2,6 +2,6 @@ function [w] = weight_kimura(e)
     %Function that calculates the weight of the edge based on the kimura
     %model
     [alpha, beta] = get_params(e,100,1000);
-    [P,Q,R] = sim_kimura_3(alpha,beta, beta,1);
+    [P,Q,R] = sim_kimura_3(alpha,beta, beta,1); %Gamma = Beta to simulate the 2 parameter model 
     w = P+Q;
 end
